8feb35073607bf4e2454ba0af4208a5395a41943
gothchico <mahimaraut07@gmail.com>
2024-11-10 05:20:14 -0600

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
-export PYTHONPATH="/Users/mahimaraut/UChicago/python-seb/data-forge"
-# export PYTHONPATH="<ROOT_DIR>/data-forge"
+#!/bin/bash
+
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export PYTHONPATH="$ROOT_DIR/data-forge"
+echo "PYTHONPATH set to: $PYTHONPATH"
